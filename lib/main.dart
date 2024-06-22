00a592e226a6b72ca57624522f951a29d3b8451d
hazzem salah <hazzemsalaht6@gmail.com>
2024-06-22 18:27:03 +0300

    Create NotesVeiw

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:_9_note_app/views/notes_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: Scaffold(),
+      home: const NotesView(),
     );
   }
 }

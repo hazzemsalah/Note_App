b947b5d33d2c4bc55914cb81b05caa3ee8141f68
hazzem salah <hazzemsalaht6@gmail.com>
2024-06-23 04:08:47 +0300

    create edit note view

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const NotesApp());
 }
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
         fontFamily: "Poppins",
--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,3 +1,4 @@
+import 'package:_9_note_app/views/widgets/edit_note_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -6,56 +7,68 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 14),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              "First Note",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteViewBody();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 14),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                "First Note",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 18),
+                child: Text(
+                  "This is a subtitle for this note",
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.5),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: Padding(
+                padding: const EdgeInsets.only(bottom: 32),
+                child: IconButton(
+                  padding: const EdgeInsets.only(left: 16),
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
+                  ),
+                ),
               ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 18),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
               child: Text(
-                "This is a subtitle for this note",
+                "June 22 , 2024",
                 style: TextStyle(
                   color: Colors.black.withOpacity(0.5),
-                  fontSize: 18,
                 ),
               ),
             ),
-            trailing: Padding(
-              padding: const EdgeInsets.only(bottom: 32),
-              child: IconButton(
-                padding: const EdgeInsets.only(left: 16),
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 24,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              "June 22 , 2024",
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.5),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
